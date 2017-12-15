b1eaad25372194d5e2008850f4554490b1b63d0e
Badrun Bandi <32830161+badrun-bandi@users.noreply.github.com>
2017-12-15 12:33:26 +0800

    .

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
-
+ 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
